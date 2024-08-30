56db091720e65e38d479ca25015b87b2687d53ed
Yoko Harada <yokolet@gmail.com>
2024-08-30 21:50:29 +0900

    installs fontawesome

--- app/frontend/entrypoints/application.ts
+++ app/frontend/entrypoints/application.ts
@@ -33,11 +33,19 @@ import '../assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+library.add(fas, far, fab)
+
 import App from '../App.vue'
 import router from '../router'
 
 const app = createApp(App)
 
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.use(createPinia())
 app.use(router)
 
